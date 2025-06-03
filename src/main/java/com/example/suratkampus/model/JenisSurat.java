1eb363c846603a0cfa169d24d8d28cc28de0d593
riccikoo <reivalaura@gmail.com>
2025-06-03 14:58:03 +0700

    add new jenis surat

--- src/main/java/com/example/suratkampus/model/JenisSurat.java
+++ src/main/java/com/example/suratkampus/model/JenisSurat.java
@@ -3,6 +3,9 @@ package com.example.suratkampus.model;
 public enum JenisSurat {
     KETERANGAN_AKTIF,
     CUTI,
-    RISET
+    RISET,
+    KETERANGAN_PERNAH_KULIAH,
+    DISPENSASI,
+    REKOMENDASI,
+    PKL_MAGANG,
 }
-
